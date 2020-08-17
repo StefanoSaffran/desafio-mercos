cca9e9d5ad58c230bdeb484f5b5411d08b522084
Stefano Saffran <stefanosaffran@hotmail.com>
2020-08-17 21:39:13 +0200

    feat: add order Summary implementation

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 import Header from '../../components/Header';
+import CartItem from '../../components/CartItem';
 import ShoppingInfosBanner from '../../components/ShoppingInfosBanner';
 
-import { Container } from './styles';
+import {
+  Container,
+  CartWrapper,
+  CartList,
+  Summary,
+  Details,
+  DetailsItem,
+  Total,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -11,6 +20,45 @@ const Dashboard: React.FC = () => {
       <Header />
 
       <ShoppingInfosBanner />
+
+      <h1>Carrinho</h1>
+
+      <CartWrapper>
+        <CartList>
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </CartList>
+
+        <Summary>
+          <header>
+            <span>Resumo do pedido</span>
+          </header>
+
+          <Details>
+            <DetailsItem>
+              <span>Itens</span>
+              <span>5</span>
+            </DetailsItem>
+            <DetailsItem>
+              <span>Total em produtos</span>
+              <span>R$ 62,50</span>
+            </DetailsItem>
+            <DetailsItem>
+              <span>Descontos</span>
+              <span>R$ 0,00</span>
+            </DetailsItem>
+
+            <Total>
+              <span>Total</span>
+              <span>R$ 10,00</span>
+            </Total>
+
+            <button type="button">Finalizar a compra</button>
+          </Details>
+        </Summary>
+      </CartWrapper>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,4 +3,80 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100%;
   width: 100%;
+
+  h1 {
+    text-align: center;
+    font-weight: 500;
+    color: var(--color-text-primary);
+    margin: 30px 0;
+  }
+`;
+
+export const CartWrapper = styled.div`
+  height: 100%;
+  width: min(100%, 1070px);
+
+  margin: 0 auto;
+
+  display: flex;
+`;
+
+export const CartList = styled.ul`
+  flex: 1;
+`;
+
+export const Summary = styled.div`
+  width: 320px;
+  border: 1px solid var(--color-border);
+
+  > header {
+    padding: 15px 18px;
+    border-bottom: 1px solid var(--color-border);
+
+    > span {
+      font-size: 1.4rem;
+      font-weight: 500;
+      text-transform: uppercase;
+    }
+  }
+`;
+
+export const Details = styled.div`
+  padding: 14px 18px 18px;
+
+  > button {
+    background: var(--color-primary);
+    color: #fff;
+    font-weight: 500;
+    width: 100%;
+    padding: 16px;
+    text-align: center;
+  }
+`;
+
+export const DetailsItem = styled.div`
+  font-size: 1.4rem;
+  color: var(--color-text-secondary);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 13px 0;
+  line-height: 1.25;
+
+  span + span {
+    font-weight: 500;
+  }
+`;
+
+export const Total = styled.div`
+  color: var(--color-text-primary);
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 30px 0 25px;
 `;
