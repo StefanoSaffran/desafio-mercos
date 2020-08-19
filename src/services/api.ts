fcfb6e5ab2a142dbccc060cdeaee2da14e0423bf
Stefano Saffran <stefanosaffran@hotmail.com>
2020-08-19 18:05:54 +0200

    fix: workaround for cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const originalURL = 'https://sandbox.mercos.com/api-teste-front';
+const queryURL = `https://cors-anywhere.herokuapp.com/${originalURL}`;
+
 const api = axios.create({
-  baseURL: 'https://sandbox.mercos.com/api-teste-front',
+  baseURL: queryURL,
 });
 
 export default api;
